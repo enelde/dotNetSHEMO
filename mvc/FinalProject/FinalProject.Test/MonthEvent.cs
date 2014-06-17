02ddae39a354fd505dce8226be050f8479ccfcd1
enelde <sushierz.sashimierz@gmail.com>
2014-06-17 17:00:15 +0700

    KErjaan bersama

---
+++ mvc/FinalProject/FinalProject.Test/MonthEvent.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FinalProject;
+using FinalProject.Controllers;
+using FinalProject.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+
+
+namespace FinalProject.Test
+{
+    [TestClass]
+    public class MonthEvent
+    {
+        MonthEventController me = new MonthEventController();
+        SHEMOEntities1 db = new SHEMOEntities1();
+
+        [TestInitialize]
+        public void Init()
+        {
+            me = new MonthEventController();
+        }
+
+        [TestMethod]
+        public void Search_Test()
+        {
+            var tesList = from t in db.EVENTs
+                          where t.Tanggal_Mulai.Month.Equals(5) ||
+                                t.Tanggal_Selesai.Month.Equals(5)
+                          select t;
+
+            var cari = "May";
+
+            Assert.AreEqual(cari, tesList.Month());
+        }
+
+
+
+
+    }
+}
